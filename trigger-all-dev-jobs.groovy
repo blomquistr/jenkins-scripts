15309579b59b66847338e4d1be70cab2780326ad
Ryan Blomquist <=>
2019-04-29 18:44:16 -0500

    Updating the loop to match Cloudbees suggested

--- trigger-all-dev-jobs.groovy
+++ trigger-all-dev-jobs.groovy
@@ -3,10 +3,12 @@
 * defined in the variable pattern at the top of the script. Change the
 * pattern to be able to bulk execute Develop jobs from the script console
 */
-def pattern = /Dev-and-PR-Pipelines/
+def pattern = /\/Dev-and-PR-Pipelines\/.*\/master/
 
 Jenkins.instance.getAllItems(AbstractItem.class).each {
-    if (assert it.fullName =~ pattern) {
+    def matches = it.fullName =~ pattern
+    if (matches.size() > 0) {
+        println("Scheduling build for ${it.fullName}")
         it.scheduleBuild()
     }
 };
